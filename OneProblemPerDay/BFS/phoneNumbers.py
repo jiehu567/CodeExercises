c3c71d90b839ca0fd76577e6d338cc199a6eb8da
James Hu <hujams@amazon.com>
2019-06-01 21:01:31 -0700

    optimized phoneNumber;

--- OneProblemPerDay/BFS/phoneNumbers.py
+++ OneProblemPerDay/BFS/phoneNumbers.py
@@ -1,34 +1,61 @@
 # BFS version:
-class Solution:
 
+# Optimized Solution, use array to replace dict
+
+class Solution:
+    """
+    @param digits: A digital string
+    @return: all posible letter combinations
+    """
     def combinations(self, digits):
-        
+        # write your code here
         if not digits:
             return []
-        
-        d = {
-            '2': ['a','b','c'],
-            '3': ['d','e','f'],  
-            '4': ['g','h','i'], 
-            '5': ['j','k','l'], 
-            '6': ['m','n','o'], 
-            '7': ['p','q','r','s'], 
-            '8': ['t','u','v'], 
-            '9': ['w','x','y', 'z']      
-            }
-        
-        res = []
+        phone = [None, None, 'abc', 'def', 'ghi', 'jkl','mno','pqrs','tuv','wxyz']
         from collections import deque
         q = deque()
         q.append('')
+        res = []
         while q:
             cur = q.popleft()
             if len(cur) == len(digits):
                 res.append(cur)
             else:
-                for c in d[digits[len(cur)]]:
-                    q.append(cur + c)
+                for c in phone[int(digits[len(cur)])]:
+                    q.append(cur+c)
         return res
+
+# Traditional solution using dict
+# class Solution:
+
+#     def combinations(self, digits):
+        
+#         if not digits:
+#             return []
+        
+#         d = {
+#             '2': ['a','b','c'],
+#             '3': ['d','e','f'],  
+#             '4': ['g','h','i'], 
+#             '5': ['j','k','l'], 
+#             '6': ['m','n','o'], 
+#             '7': ['p','q','r','s'], 
+#             '8': ['t','u','v'], 
+#             '9': ['w','x','y', 'z']      
+#             }
+        
+#         res = []
+#         from collections import deque
+#         q = deque()
+#         q.append('')
+#         while q:
+#             cur = q.popleft()
+#             if len(cur) == len(digits):
+#                 res.append(cur)
+#             else:
+#                 for c in d[digits[len(cur)]]:
+#                     q.append(cur + c)
+#         return res
 
 # Test case
 s = Solution()
